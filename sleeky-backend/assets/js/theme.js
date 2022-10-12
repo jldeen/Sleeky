390f2cd108ef712d19f843febf793474f210cbc6
Jessica Deen <jessicadeen@me.com>
2022-10-11 23:18:34 -0700

    init commit
    Signed-off-by: Jessica Deen <jessicadeen@me.com>

--- sleeky-backend/assets/js/theme.js
+++ sleeky-backend/assets/js/theme.js
@@ -3,11 +3,11 @@
 
 console.log("Sleeky Admin Theme Running");
 
-$( document ).ready(function() {
+$(document).ready(function () {
   // Get the theme URL
   var url;
-  if ($('meta[name=pluginURL]').attr("content")) {
-    url = $('meta[name=pluginURL]').attr("content");
+  if ($("meta[name=pluginURL]").attr("content")) {
+    url = $("meta[name=pluginURL]").attr("content");
   } else {
     // If for some reason we can't find the URL attribute
     url = "/user/plugins/sleeky_backend";
@@ -15,19 +15,21 @@ $( document ).ready(function() {
 
   // Detect theme
   var theme;
-  if ($('meta[name=sleeky_theme]').attr("content") == 'light') {
+  if ($("meta[name=sleeky_theme]").attr("content") == "light") {
     theme = "light";
-  } else if ($('meta[name=sleeky_theme]').attr("content") == 'dark') {
+  } else if ($("meta[name=sleeky_theme]").attr("content") == "dark") {
     theme = "dark";
   }
-  
-  console.log("Theme is", theme)
+
+  console.log("Theme is", theme);
 
   // Update favicon
-  $('link[rel="shortcut icon"]').attr('href', url + "/assets/img/favicon.ico");
+  $('link[rel="shortcut icon"]').attr("href", url + "/assets/img/favicon.ico");
 
   // Update meta viewport
-  $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+  $("head").append(
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+  );
 
   // Detect pages
   if ($("body").hasClass("login")) {
@@ -35,17 +37,19 @@ $( document ).ready(function() {
     console.log("Login page");
 
     if (theme == "light") {
-      $("#login").prepend(`<img class="login-logo" src="${url}/assets/img/logo_black.png">`);
+      $("#login").prepend(
+        `<img class="login-logo" src="${url}/assets/img/logo_black.png">`
+      );
     } else if (theme == "dark") {
-      $("#login").prepend(`<img class="login-logo" src="${url}/assets/img/logo_white.png">`);
+      $("#login").prepend(
+        `<img class="login-logo" src="${url}/assets/img/ecs.png">`
+      );
     }
-
-    
   } else if ($("body").hasClass("index")) {
     // Index page
     console.log("Index page");
 
-    handleNav()
+    handleNav();
 
     // Hide YOURLS new URL section
     $("#new_url").hide();
@@ -57,72 +61,77 @@ $( document ).ready(function() {
     $("#new_url").remove();
 
     // Create the sleeky new URL section from the template
-    $("nav").append($('<div>').load(`${url}/assets/html/form.html`, function () {
-      $("#nonce-add").val(nonce);
-    }));
+    $("nav").append(
+      $("<div>").load(`${url}/assets/html/form.html`, function () {
+        $("#nonce-add").val(nonce);
+      })
+    );
   } else if ($("body").hasClass("tools")) {
     // Tools page
     console.log("Tools page");
 
-    handleNav()
-
+    handleNav();
   } else if ($("body").hasClass("plugins")) {
     // Plugins page
     console.log("Plugins page");
 
-    handleNav()
-
+    handleNav();
   } else if ($("body").hasClass("plugin_page_sleeky_settings")) {
     // Tools page
     console.log("Sleeky Settings Page");
 
-    handleNav()
+    handleNav();
 
     $("#ui_selector").val($("#ui_selector").attr("value"));
-
-  }  else if ($("body").hasClass("infos")) {
+  } else if ($("body").hasClass("infos")) {
     // Information page
     console.log("Information page");
 
-    handleNav()
+    handleNav();
 
     $("#historical_clicks li").each(function (index) {
       if (index % 2 != 0) {
         $("#historical_clicks li").eq(index).css("background", "");
       }
-    })
+    });
 
     // Update tab headers
-    var titles = ['Statistics', 'Location', 'Sources']
+    var titles = ["Statistics", "Location", "Sources"];
     for (let i = 0; i < 3; i++) {
-      $($('#headers > li')[i]).find('h2').text(titles[i]);
+      $($("#headers > li")[i]).find("h2").text(titles[i]);
     }
   } else {
     console.warn("Unknown page");
-    
+
     handleNav();
   }
 
   function handleNav() {
     // Add logo
-    $("#wrap").prepend(`<img class="logo" src="${url}/assets/img/logo_white.png">`);
+    $("#wrap").prepend(
+      `<img class="logo" src="${url}/assets/img/logo_white.png">`
+    );
 
     // Add mobile nav hamburger
-    $("#wrap").prepend(`<div class="nav-open" id="navOpen"><i class="material-icons">menu</i></div>`);
+    $("#wrap").prepend(
+      `<div class="nav-open" id="navOpen"><i class="material-icons">menu</i></div>`
+    );
 
     // Add frontend link
-    $('#admin_menu').append('<li class="admin_menu_toplevel frontend_link"><a href="/"><i class="material-icons">arrow_back</i> Frontend Interface</a></li>');
+    $("#admin_menu").append(
+      '<li class="admin_menu_toplevel frontend_link"><a href="/"><i class="material-icons">arrow_back</i> Frontend Interface</a></li>'
+    );
 
     // admin_menu
-    $('#navOpen').on('click', function() {
-      $('#admin_menu').slideToggle();
-    })
+    $("#navOpen").on("click", function () {
+      $("#admin_menu").slideToggle();
+    });
 
     $(window).resize(function () {
       if ($(window).width() > 899) {
-        $('#admin_menu').show();
+        $("#admin_menu").show();
       } else {
-        $('#admin_menu').hide();
+        $("#admin_menu").hide();
       }
     });
   }
@@ -134,11 +143,15 @@ $( document ).ready(function() {
       $("main").append("<p>" + $(this).html() + "</p>");
       $(this).remove();
     } else if (/Powered by/.test($(this).text())) {
-        // Update footer
-        var content = $(this).html();
-        var i = 77
-        var updated_content = "Running on" + content.slice(13, i) + '& <a href="https://sleeky.flynntes.com/" title="Sleeky">Sleeky</a>' + content.slice(i-1)
-        $(this).html(updated_content);
-      }
+      // Update footer
+      var content = $(this).html();
+      var i = 77;
+      var updated_content =
+        "Running on" +
+        content.slice(13, i) +
+        '& <a href="https://sleeky.flynntes.com/" title="Sleeky">Sleeky</a>' +
+        content.slice(i - 1);
+      $(this).html(updated_content);
+    }
   });
 });
